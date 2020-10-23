ba930e1bf34a1969a7116a41f7318eb2ea066882
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-10-23 13:17:04 +0530

    wip

--- addon-test-support/extend/index.js
+++ addon-test-support/extend/index.js
@@ -7,19 +7,19 @@ import {
   register as registerExecutionContext
 } from '../-private/execution_context';
 
-import IntegrationNativeEventsContext from '../-private/execution_context/integration-native-events';
-import AcceptanceNativeEventsContext from '../-private/execution_context/acceptance-native-events';
+// import IntegrationNativeEventsContext from '../-private/execution_context/integration-native-events';
+// import AcceptanceNativeEventsContext from '../-private/execution_context/acceptance-native-events';
 import IntegrationEmberContext from '../-private/execution_context/integration';
 import AcceptanceEmberContext from '../-private/execution_context/acceptance';
 
 function useNativeEvents(flag = true) {
-  if (flag) {
-    registerExecutionContext('integration', IntegrationNativeEventsContext);
-    registerExecutionContext('acceptance', AcceptanceNativeEventsContext);
-  } else {
+  // if (flag) {
+  //   registerExecutionContext('integration', IntegrationNativeEventsContext);
+  //   registerExecutionContext('acceptance', AcceptanceNativeEventsContext);
+  // } else {
     registerExecutionContext('integration', IntegrationEmberContext);
     registerExecutionContext('acceptance', AcceptanceEmberContext);
-  }
+  // }
 }
 
 export { registerExecutionContext, useNativeEvents };
